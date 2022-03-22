2b9e064591f2d1593ddeb679ace919180b34dcd6
Pedro Santiago <pedrossantiago1@gmail.com>
2022-03-21 22:27:44 -0300

    mudado data do endpoint hoje

--- api/hoje.js
+++ api/hoje.js
@@ -1,10 +1,11 @@
 const clientPromise = require('../mongodb-client')
 
 export default async (req, res) => {
+	const dataSG = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60000 + 3600000 * -3)
 	const client = await clientPromise
 	const collection = client.db('DBLabvirtual').collection('Dados_Sensores')
 	try {
-		const dados = await collection.findOne({ _id: new Date().toLocaleDateString('pt-BR') })
+		const dados = await collection.findOne({ _id: dataSG.toLocaleDateString('pt-BR') })
 		return res.send(dados)
 	} catch (error) {
 		return res.json(error.message)
