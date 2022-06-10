88fdfec2694d1d25c1de84c2d32d03f4c2aee49d
Pedro Santiago <pedrossantiago1@gmail.com>
2022-06-09 21:55:34 -0300

    teste

--- api/teste.js
+++ api/teste.js
@@ -4,7 +4,7 @@ export default async (req, res) => {
 	const client = await clientPromise
 	const collection = client.db('DBLabvirtual').collection('teste')
 	try {
-		const dados = await collection.findOne({ nome: req.params.nome })
+		const dados = await collection.findOne({})
 		return res.send(dados)
 	} catch (error) {
 		return res.json(error.message)
